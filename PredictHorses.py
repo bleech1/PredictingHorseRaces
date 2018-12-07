84cbbbf7be80036d603a90e2c9183ed0b6dbd10e
Brendan Leech <Bleecher@brendans-mbp.middlebury.edu>
2018-12-07 03:41:47 -0500

    Cleaning up code and finalizing

--- PredictHorses.py
+++ PredictHorses.py
@@ -3,7 +3,7 @@
 # Colleen Caveney and Brendan Leech
 
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Dropout, Activation
 from keras.utils import to_categorical
 from itertools import product
 from sklearn.metrics import classification_report
@@ -12,123 +12,9 @@ import pandas as pd
 import numpy as np
 import csv
 import math
+import tensorflow as tf
 
-HORSE_INFO_FILE = "./horse-racing-dataset-for-experts-hong-kong/horse_info.csv"
-RESULTS_FILE = "./horse-racing-dataset-for-experts-hong-kong/results.csv"
-BARRIER_FILE = "./horse-racing-dataset-for-experts-hong-kong/barrier.csv"
-OUTFILE = "combined_data.csv"
-
-
-def JoinData(horseInfoFile, resultsFile, barrierFile, outFile):
-    # Combine the results and barrier files
-    results = pd.read_csv(resultsFile)
-    barrier = pd.read_csv(barrierFile)
-    merged = pd.concat([results, barrier], axis = 0, ignore_index = True)
-
-    # Join the combined file and the horse info file on the horse's name
-    horseInfo = pd.read_csv(horseInfoFile)
-    merged = merged.merge(horseInfo, on = "horse")
-
-    # Shuffle the rows in the dataset
-    merged = merged.reindex(np.random.permutation(merged.index))
-
-    # Write out to a CSV file
-    merged.to_csv(outFile, index = False)
-
-
-def AddCategoricalFeature(filename, feature):
-    df = pd.read_csv(filename)
-    featureValues = set()
-
-    # Get all of the unique horse names
-    for index, row in df.iterrows():
-        #print(row[feature])
-        featureValues.add(row[feature])
-
-    featureList = list(featureValues)
-
-    # Make column of values for whether a horse name = given horse name
-    count = 1
-    for horseName in featureList:
-        horseNameCol = []
-        print(count, "/", len(featureList))
-        for index, row in df.iterrows():
-            name = row[feature]
-            if (horseName == name):
-                horseNameCol += [1]
-            else:
-                horseNameCol += [0]
-        df[feature + horseName] = pd.Series(horseNameCol, index = df.index)
-        count += 1
-    df.to_csv(filename, index = False)
-
-def AddNumberOpponents(filename):
-    df = pd.read_csv(filename)
-
-    # For each horse's race,
-    length = len(df)
-    numOpponents = dict()
-    for index, row in df.iterrows():
-        # Find the number of opponents for the horse
-        identifier = str(row["date"]) + str(row["raceno"]) + str(row["venue"])
-        if identifier in numOpponents:
-            numOpponents[identifier] += 1
-        else:
-            numOpponents[identifier] = 1
-    horseNameCol = []
-    for index, row in df.iterrows():
-        identifier = str(row["date"]) + str(row["raceno"]) + str(row["venue"])
-        horseNameCol += [numOpponents[identifier]]
-    # Add the column to the dataset
-    df["numOpponents"] = pd.Series(horseNameCol, index = df.index)
-    df.to_csv(filename, index = False)
-
-
-def CategoricalFeatureToNum(filename, featureName):
-    categoricalValues = []
-    numericalValues = []
-    indexHorse = 0
-    df = pd.read_csv(filename)
-    for index, row in df.iterrows():
-        feature = str(row[featureName])
-        if (len(feature) != 0):
-            feature = feature.lower()
-        if(feature in categoricalValues):
-            numericalValues.append(categoricalValues.index(feature))
-        else:
-            categoricalValues.append(feature)
-            numericalValues.append(indexHorse)
-            indexHorse += 1
-
-    print(len(set(numericalValues)))
-
-    df[featureName + "Numerical"] = pd.Series(numericalValues, index=df.index)
-    df.to_csv(filename, index=False)
-
-def AddNumTimesWon(filename, featureName):
-    df = pd.read_csv(filename)
-    values = []
-
-    numTimesWon = dict()
-    # Go through dataset and find how many times each has won
-    for index, row in df.iterrows():
-        feature = str(row[featureName])
-        if (row["plc"] == "1"):
-            if feature in numTimesWon:
-                numTimesWon[feature] += 1
-            else:
-                numTimesWon[feature] = 1
-    # Fill in feature array with how many times it has won
-    for index, row in df.iterrows():
-        feature = str(row[featureName])
-        if (feature in numTimesWon):
-            values += [numTimesWon[feature]]
-        else:
-            values += [0]
-
-    df[featureName + "WinTimes"] = pd.Series(values, index=df.index)
-    df.to_csv(filename, index=False)
-
+DATAFILE = "combined_data.csv"
 
 def CreateDataSets(combinedFilename):
     features = []
@@ -139,44 +25,32 @@ def CreateDataSets(combinedFilename):
         # First row is headers
         headers = next(csvReader)
         # Only take the headers that we care about (look below for explanation)
-        headers = [headers[10]] + [headers[25]] + [headers[36]] + headers[50:] + [headers[49]]
+        headers = [headers[36]] + headers[49:]
 
         for row in csvReader:
-            # 0th element is an index value we can ignore
-
-            # Just choosing 2 float values for our parameters at the beginning
-            # Skip if the value doesn't exist
-
-            # 2nd element is the place (our label)
-            # We are classifying as first, second, third, or worse places
-            # So worse than third place will be represented as 0
+            # If the horse did not finish in top 3, label 0
             if (row[24] not in ["1", "2", "3"]):
                 labels.append(0)
             else:
-                labels.append(int(row[24]))
-            #labels.append(int(row[49]))
+                # If finished in top 3, label 1
+                labels.append(1)
 
-            # If empty, fill in winodds with 30.122 because that is the mean
-            # of the winodds for the horses that do have odds
-            if (row[36] == ""):
+            # column 36 is the betting odds
+            if row[36] == "":
+                # If horse did not have odds, then make the odds 30.122, which
+                # if the average for horses that did have odds
                 row[36] = "30.122"
 
-            if (row[24] == "" or not (row[24][0].isdigit() and row[24][-1].isdigit())):
-                row[24] = "4"
-
-            row = [row[10]] + [row[25]] + [row[36]] + row[50:] + [row[49]]
+            # Everything from 49 on are the features that we added to the model
+            row = [row[36]] + row[49:]
 
             arr = np.array(row)
-            #print(arr)
             arr = np.asfarray(arr)
             features.append(np.array(arr))
 
     matrix = np.vstack(features)
 
-
-
-
-    # Split into train, cv, and test sets
+    # Split into train, cv, and test sets (70/15/15 split)
     numTrain = math.floor(matrix.shape[0] * 0.7)
     numCv = math.floor(matrix.shape[0] * 0.15)
     trainData = matrix[:numTrain]
@@ -187,167 +61,93 @@ def CreateDataSets(combinedFilename):
     testLabels = labels[numTrain + numCv + 1:]
     return trainData, trainLabels, cvData, cvLabels, testData, testLabels, headers
 
-def CreateModel(numInputFeatures):
-    # Create a model
-    model = Sequential()
-    model.add(Dense(10, activation = "relu", input_dim = numInputFeatures))
-    model.add(Dense(33, activation = "softmax"))
-    return model
-
-def get_categorical_accuracy_keras(y_true, y_pred):
-    return K.mean(K.equal(K.argmax(y_true, axis=1), K.argmax(y_pred, axis=1)))
-
-def CompileModel(model):
-    # Compile the model
-    model.compile(optimizer = "rmsprop", loss = "mean_squared_error", metrics = [get_categorical_accuracy_keras])
-    return model
-
-def TrainModel(model, trainData, trainLabels):
-    # Turn the labels into one hot labels
-    oneHotLabels = to_categorical(trainLabels, num_classes = 33)
-    # Train the model on the training data
-    history = model.fit(trainData, oneHotLabels, epochs = 10, batch_size = 100, verbose = 1)
-    return model, history
-
-def EvaluateModel(model, testData, testLabels):
-    # Evaluate the model on the testing set
-    oneHotLabels = to_categorical(testLabels, num_classes = 33)
-    score = model.evaluate(testData, oneHotLabels)
-    print(score)
-
-
-def PrintPrecisionAccuracy(model, testData, testLabels):
-    y_pred = model.predict_classes(testData)
-    print(classification_report(testLabels, y_pred))
-
+def PrintPrecisionAccuracy(model, cvData, cvLabels):
+    y_pred = model.predict_classes(cvData)
+    print(classification_report(cvLabels, y_pred))
 
 def BetOnRaces(model, cvData, cvLabels):
-    # Go through each race that has a horse's odds (30.122 is the average
-    # that we used to fill in missing values)
-    for row in cvData:
-        if (row[2] == 30.122):
+    withoutModelWinnings = 0
+    withModelWinnings = 0
+
+    # Go through each example from the cv set
+    for i in range(len(cvData)):
+        # Skip the horses that did not originally have odds
+        if (cvData[i][0] == 30.122):
             continue
         # Run model on all the examples with real odds
-        row = np.array([row])
+        row = np.array([cvData[i]])
         result = model.predict(row)
-        #print(result)
+        # Odds for a show are roughly half of win odds
+        showOdds = row[0, 0] / 2
+
+        # Betting strategies
+        # Without model: bet $100 on favorites to show
+        # With model: bet $100 on all horses we predict with at least probability
+        # 90% will show
+
+        # Is favorite, so w/o model bet 100
+        if row[0, 1] == 1:
+            if cvLabels[i] == 1:
+                withoutModelWinnings += (100 * showOdds)
+            else:
+                withoutModelWinnings -= 100
+        # Predict show, so w/ model bet 100
+        if result[0] > 0.9:
+            if cvLabels[i] == 1:
+                withModelWinnings += (100 * showOdds)
+            else:
+                withModelWinnings -= 100
+
+    print("Without model winnings:", withoutModelWinnings)
+    print("With model winnings:", withModelWinnings)
 
 
-print("start")
-JoinData(HORSE_INFO_FILE, RESULTS_FILE, BARRIER_FILE, OUTFILE)
-print("joined")
-# AddCategoricalFeature(OUTFILE, "horse")
+# From: https://stackoverflow.com/questions/43076609/how-to-calculate-precision-and-recall-in-keras
+def as_keras_metric(method):
+    import functools
+    from keras import backend as K
+    import tensorflow as tf
+    @functools.wraps(method)
+    def wrapper(self, args, **kwargs):
+        """ Wrapper for turning tensorflow metrics into keras metrics """
+        value, update_op = method(self, args, **kwargs)
+        K.get_session().run(tf.local_variables_initializer())
+        with tf.control_dependencies([update_op]):
+            value = tf.identity(value)
+        return value
+    return wrapper
 
-#CategoricalFeatureToNum(OUTFILE, "plc")
+precision = as_keras_metric(tf.metrics.precision)
+recall = as_keras_metric(tf.metrics.recall)
 
-# Get the other horses in the race
-AddNumberOpponents(OUTFILE)
+def f1Score(y_true, y_pred):
+    # Calcuates the F1 score from precision and accuracy
+    p = precision(y_true, y_pred)
+    r = recall(y_true, y_pred)
 
-# Convert string features to numbers
-print("Adding numerical horse variable")
-CategoricalFeatureToNum(OUTFILE, "horse")
-print("Adding numerical trainer variable")
-CategoricalFeatureToNum(OUTFILE, "trainer_x")
-print("Adding numerical country variable")
-CategoricalFeatureToNum(OUTFILE, "country")
-print("Adding numerical sire variable")
-CategoricalFeatureToNum(OUTFILE, "sire")
-# print("Adding numerical dam variable")
-# CategoricalFeatureToNum(OUTFILE, "dam")
-# print("Adding numerical owner variable")
-# CategoricalFeatureToNum(OUTFILE, "owner")
-print("Adding numerical sex variable")
-CategoricalFeatureToNum(OUTFILE, "sex")
-print("Adding numerical course variable")
-CategoricalFeatureToNum(OUTFILE, "course")
-# print("Adding numerical date variable")
-# CategoricalFeatureToNum(OUTFILE, "date")
-print("Adding numerical going variable")
-CategoricalFeatureToNum(OUTFILE, "going")
-print("Adding numerical venue variable")
-CategoricalFeatureToNum(OUTFILE, "venue")
-print("Adding numerical jockey variable")
-CategoricalFeatureToNum(OUTFILE, "jockey")
-print("Adding numerical import variable")
-CategoricalFeatureToNum(OUTFILE, "import_type")
-
-# Add features for number of times horse/trainer/jockey has won
-AddNumTimesWon(OUTFILE, "horse")
-AddNumTimesWon(OUTFILE, "trainer_x")
-AddNumTimesWon(OUTFILE, "jockey")
+    return 2 * (p * r) / (p + r)
 
 
-trainData, trainLabels, cvData, cvLabels, testData, testLabels, headers = CreateDataSets(OUTFILE)
-print(testData)
+# Split the datasets
+trainData, trainLabels, cvData, cvLabels, testData, testLabels, headers = CreateDataSets(DATAFILE)
 print("created data sets")
 numInputFeatures = len(headers)
-print(numInputFeatures)
+print("Num input features:", numInputFeatures)
 
-
-
-
+# Define the model
 model = Sequential()
 model.add(Dense(30, activation = "relu", input_dim = numInputFeatures))
-model.add(Dense(30, activation = "relu"))
-model.add(Dense(30, activation = "relu"))
-model.add(Dense(4, activation = "softmax"))
+model.add(Dense(1, activation = "sigmoid"))
 
+model.compile(optimizer = "rmsprop", loss = "binary_crossentropy", metrics = ["accuracy", precision, recall, f1Score])
 
-def w_categorical_crossentropy(y_true, y_pred):
-    # Customize weight array to punish for misclassifying 1, 2, or 3 as a 1
-    weights = np.ones((4, 4))
-    weights[1, 0] = 1.5
-    weights[2, 0] = 1.5
-    weights[3, 0] = 1.5
-    weights[0, 1] = 1.5
-    weights[0, 2] = 1.5
-    weights[0, 3] = 1.5
-
-
-
-    nb_cl = len(weights)
-    final_mask = K.zeros_like(y_pred[:, 0])
-    y_pred_max = K.max(y_pred, axis=1)
-    y_pred_max = K.expand_dims(y_pred_max, 1)
-    y_pred_max_mat = K.equal(y_pred, y_pred_max)
-    for c_p, c_t in product(range(nb_cl), range(nb_cl)):
-        final_mask += (K.cast(weights[c_t, c_p],K.floatx()) * K.cast(y_pred_max_mat[:, c_p] ,K.floatx())* K.cast(y_true[:, c_t],K.floatx()))
-    return K.categorical_crossentropy(y_pred, y_true) * final_mask
-
-#WeightedLoss = w_categorical_crossentropy
-#WeightedLoss.__name__ ='w_categorical_crossentropy'
-
-model.compile(optimizer = "rmsprop", loss = w_categorical_crossentropy, metrics = [get_categorical_accuracy_keras, "accuracy"])
-
-oneHotLabels = to_categorical(trainLabels, num_classes = 4)
 # Train the model on the training data
-history = model.fit(trainData, oneHotLabels, epochs = 10, batch_size = 100, verbose = 1)
+history = model.fit(trainData, trainLabels, epochs = 100, batch_size = 100, verbose = 1)
 
-oneHotLabels = to_categorical(testLabels, num_classes = 4)
-score = model.evaluate(testData, oneHotLabels, batch_size = 100)
+# Evaluate and print out testing data
+score = model.evaluate(testData, testLabels, batch_size = 100)
 print(score)
 
-
-
-
-
-
-
-#
-# model = CreateModel(numInputFeatures)
-# print("created model")
-# model = CompileModel(model)
-# print("compiled model")
-# model, history = TrainModel(model, trainData, trainLabels)
-#
-# print(history)
-#
-# print("trained model")
-# EvaluateModel(model, cvData, cvLabels)
-# print("evaluated")
-#
-# print("predictions made")
 PrintPrecisionAccuracy(model, cvData, cvLabels)
-
 
 BetOnRaces(model, cvData, cvLabels)
--- TrainModel.py
+++ TrainModel.py
@@ -28,28 +28,29 @@ def CreateDataSets(combinedFilename):
         headers = [headers[36]] + headers[49:]
 
         for row in csvReader:
+            # If the horse did not finish in top 3, label 0
             if (row[24] not in ["1", "2", "3"]):
                 labels.append(0)
             else:
-                #labels.append(int(row[24]))
+                # If finished in top 3, label 1
                 labels.append(1)
 
+            # column 36 is the betting odds
             if row[36] == "":
+                # If horse did not have odds, then make the odds 30.122, which
+                # if the average for horses that did have odds
                 row[36] = "30.122"
 
+            # Everything from 49 on are the features that we added to the model
             row = [row[36]] + row[49:]
 
             arr = np.array(row)
-            #print(arr)
             arr = np.asfarray(arr)
             features.append(np.array(arr))
 
     matrix = np.vstack(features)
 
-
-
-
-    # Split into train, cv, and test sets
+    # Split into train, cv, and test sets (70/15/15 split)
     numTrain = math.floor(matrix.shape[0] * 0.7)
     numCv = math.floor(matrix.shape[0] * 0.15)
     trainData = matrix[:numTrain]
@@ -60,61 +61,29 @@ def CreateDataSets(combinedFilename):
     testLabels = labels[numTrain + numCv + 1:]
     return trainData, trainLabels, cvData, cvLabels, testData, testLabels, headers
 
-def get_categorical_accuracy_keras(y_true, y_pred):
-    return K.mean(K.equal(K.argmax(y_true, axis=1), K.argmax(y_pred, axis=1)))
-
 def PrintPrecisionAccuracy(model, cvData, cvLabels):
     y_pred = model.predict_classes(cvData)
     print(classification_report(cvLabels, y_pred))
 
-def w_categorical_crossentropy(y_true, y_pred):
-    # Customize weight array to punish for misclassifying 1, 2, or 3 as a 1
-    # weights = np.ones((4, 4))
-    # weights[1, 0] = 70
-    # weights[2, 0] = 70
-    # weights[3, 0] = 70
-    weights = np.ones((2, 2))
-    weights[1, 0] = 70
-    weights[0, 0] = 0
-    weights[1, 1] = 0
-
-    nb_cl = len(weights)
-    final_mask = K.zeros_like(y_pred[:, 0])
-    y_pred_max = K.max(y_pred, axis=1)
-    y_pred_max = K.expand_dims(y_pred_max, 1)
-    y_pred_max_mat = K.equal(y_pred, y_pred_max)
-    for c_p, c_t in product(range(nb_cl), range(nb_cl)):
-        final_mask += (K.cast(weights[c_t, c_p],K.floatx()) * K.cast(y_pred_max_mat[:, c_p] ,K.floatx())* K.cast(y_true[:, c_t],K.floatx()))
-    return K.categorical_crossentropy(y_pred, y_true) * final_mask
-
-
 def BetOnRaces(model, cvData, cvLabels):
-    # Go through each race that has a horse's odds (30.122 is the average
-    # that we used to fill in missing values)
-    skipCount = 0
-    showCount = 0
-    noCount = 0
-
     withoutModelWinnings = 0
     withModelWinnings = 0
 
+    # Go through each example from the cv set
     for i in range(len(cvData)):
+        # Skip the horses that did not originally have odds
         if (cvData[i][0] == 30.122):
-            skipCount += 1
             continue
         # Run model on all the examples with real odds
         row = np.array([cvData[i]])
         result = model.predict(row)
-        # Odds for a show are normally about half of win odds
+        # Odds for a show are roughly half of win odds
         showOdds = row[0, 0] / 2
-        if result[0] > 0.5:
-            showCount += 1
-        else:
-            noCount += 1
 
         # Betting strategies
         # Without model: bet $100 on favorites to show
-        # With model: bet $100 on all horses we predict will show
+        # With model: bet $100 on all horses we predict with at least probability
+        # 90% will show
 
         # Is favorite, so w/o model bet 100
         if row[0, 1] == 1:
@@ -123,22 +92,17 @@ def BetOnRaces(model, cvData, cvLabels):
             else:
                 withoutModelWinnings -= 100
         # Predict show, so w/ model bet 100
-        #print(result)
-        if result[0] > 0.8:
+        if result[0] > 0.9:
             if cvLabels[i] == 1:
                 withModelWinnings += (100 * showOdds)
             else:
                 withModelWinnings -= 100
 
-    # print("Skipped:", skipCount, "/", len(cvData))
-    # print("Show:", showCount, "/", len(cvData))
-    # print("Not show:", noCount, "/", len(cvData))
-
     print("Without model winnings:", withoutModelWinnings)
     print("With model winnings:", withModelWinnings)
 
 
-
+# From: https://stackoverflow.com/questions/43076609/how-to-calculate-precision-and-recall-in-keras
 def as_keras_metric(method):
     import functools
     from keras import backend as K
@@ -157,34 +121,32 @@ precision = as_keras_metric(tf.metrics.precision)
 recall = as_keras_metric(tf.metrics.recall)
 
 def f1Score(y_true, y_pred):
+    # Calcuates the F1 score from precision and accuracy
     p = precision(y_true, y_pred)
     r = recall(y_true, y_pred)
 
     return 2 * (p * r) / (p + r)
 
 
-
+# Split the datasets
 trainData, trainLabels, cvData, cvLabels, testData, testLabels, headers = CreateDataSets(DATAFILE)
 print("created data sets")
 numInputFeatures = len(headers)
 print("Num input features:", numInputFeatures)
 
-
+# Define the model
 model = Sequential()
 model.add(Dense(30, activation = "relu", input_dim = numInputFeatures))
 model.add(Dense(1, activation = "sigmoid"))
 
-
 model.compile(optimizer = "rmsprop", loss = "binary_crossentropy", metrics = ["accuracy", precision, recall, f1Score])
 
-#oneHotLabels = to_categorical(trainLabels, num_classes = 2)
 # Train the model on the training data
 history = model.fit(trainData, trainLabels, epochs = 100, batch_size = 100, verbose = 1)
 
-#oneHotLabels = to_categorical(testLabels, num_classes = 2)
+# Evaluate and print out testing data
 score = model.evaluate(testData, testLabels, batch_size = 100)
 print(score)
-
 
 PrintPrecisionAccuracy(model, cvData, cvLabels)
 
